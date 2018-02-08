a8b356baad109ecf4b7f034077a3abee94d5006c
Julie Dursema <jmdursema@gmail.com>
2018-02-07 19:34:11 -0700

    write endpoint to POST and DELETE candidate

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -49,6 +50,47 @@ app.get('/api/v1/candidates/:committeeId/contributors', (request, response) => {
         error: `Count not find contributors for candadite with commmitte id of ${request.params.committeeId}`
       })
     }
+  })
+  .catch(error => {
+    return response.status(500).json({
+      error
+    })
+  })
+})
+
+app.post('/api/v1/candidates', (request, response) => {
+  
+  const candidate = request.body
+
+  for(let requiredParameter of ['committee_id', 'last_name', 'full_name', 'candidate_id', 'committee_name', 'party', 'active']){
+
+    if(!candidate[requiredParameter]){
+
+      return response.status(422).json({
+        error: `You are missing the required parameter ${requiredParameter}`
+      })
+    }
+  }
+  database('candidates').insert(candidate, 'id')
+  .then(candidate => {
+    return response.status(201).json({
+      id: candidate[0]
+    })
+  })
+
+  .catch( error => {
+    return response.status(500).json({
+      error
+    })
+  })
+})
+
+app.delete('/api/v1/candidate/:candidateId', (request, response) => {
+
+  database('candidates').where('id', request.params.candidateId).delete()
+
+  .then(candidate => {
+    return response.sendStatus(202)
   })
   .catch(error => {
     return response.status(500).json({
