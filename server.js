9de9462818c6ecae392f6a04ec58af5bbd013795
Julie Dursema <jmdursema@gmail.com>
2018-02-07 18:39:56 -0700

    write endpoint to get all candidates

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,6 +15,23 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', (request, response) => {
   response.send('Hello World!');
 });
+
+//get all candidates
+app.get('/api/v1/candidates', (request, response) => {
+
+  database('candidates').select()
+
+  .then(candidates => {
+    return response.status(200).json({
+      candidates
+    })
+  })
+  .catch(error => {
+    return response.status(500).json({
+      error
+    })
+  })
+})
 
 
 app.listen(app.get('port'), () => {
